e5bf00d59f38731b77a5b6e0baa7b00170a415d2
Ashraful Islam <ashrafulrobin3@gmail.com>
2017-08-18 18:40:52 +0600

    Added fetch for api call Added callApi method for handaling api calls Added functionality to pass data to parent component

--- api-helper.js
+++ api-helper.js
@@ -1,17 +1,66 @@
 import React from 'react';
+/* This gonna import fetch. */
 import 'whatwg-fetch';
-
 
 class APIHelper extends React.Component {
     /**
      * This is simple API Helper Object as React Component for manipulating data
-     * from API or to API. 
+     * from API or to API.
      */
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
+    componentWillMount() {
+        /**
+         * From the react architecture this method will call first when {this} component render to DOM.
+         */
+        this.callApi(this.props.apiUrl);
+    }
+
+    callApi(url) {
+        /**
+         * @param url: Actually this is api endpoint. Example: https://iashraful.me/api/v1/some-api/
+         * This method has used fetch from (https://github.com/github/fetch)
+         * fetch returns promise. This library helps to get json from promise.
+         */
+        let bodyData = undefined;
+        let meta = {
+            headers: this.props.headers,
+        };
+        if (this.props.method) {
+            meta.method = this.props.method;
+            // Simply no reason for body when it is GET method.
+            bodyData = this.props.method.toLowerCase() !== 'get' ? this.props.body : undefined;
         }
+        if (bodyData) {
+            // Only body is added when it is not GET method
+            meta.body = bodyData;
+        }
+        // fetch gonna use here...
+        let result = fetch(
+            /**
+             * First param -> url
+             * Second param -> an object for headers, method, body, etc....
+             */
+            url, meta
+        );
+
+        result.then((response) => {
+            /* Fetch will return promise here. */
+            return response.json();
+            /* From here it's returning the JSON of response data */
+        }).then((data) => {
+            /* It's time to send back the data to parent component via props */
+            this.props.apiData(data);
+        }).catch((exp) => {
+            /* If any errors occur. It will passed to action */
+            console.error(exp);
+        })
+    }
+
+    render() {
+        /**
+         * This might be worth to return null. But In this case it still need not to return anything.
+         */
+        return null;
     }
 }
+
 export default APIHelper;
